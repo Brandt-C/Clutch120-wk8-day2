ec200436c6b7d8515bcd8fbe59902d60607e394e
Brandt Carlson <brandt.carlson11@gmail.com>
2023-07-05 21:03:39 -0500

    wb work. . .  still missing edge case or two

--- dice.py
+++ dice.py
@@ -1,12 +1,46 @@
 def dice(arr):
+    def helper(arr, t):
+            while t in arr:
+                arr.remove(t)
+            return arr
+    def pair_helper(dic):
+        c = 0 
+        for v in dic.values():
+            if v == 2:
+                c += 1
+        if c == 3:
+            return True
+        return False
+    def two3(dic):
+        c = 0
+        tot = 0
+        for k, v in dic.items():
+            if v == 3:
+                c +=1
+                if k != 1:
+                    tot += k * 100
+                else:
+                    tot += 1000
+        if c == 2:
+            return tot
+        if two3(key) > 0:
+            return two3(key)
+    def last_helper(arr):
+        tot = 0
+        for a in arr:
+            if a == 1:
+                tot += 100
+            elif a == 5:
+                tot += 50
+        return tot
+    if len(arr) < 3:
+        if last_helper(arr) > 0:
+            return last_helper(arr)
+        return 'Zonk'
     if list(set(arr))==arr:
         return 1000
     key = {}
     ans = "Zonk"
-    def helper(arr, t):
-        while t in arr:
-            arr.remove(t)
-        return arr
     for a in arr:
         if a not in key:
             key[a] = 1
@@ -52,28 +86,8 @@ def dice(arr):
                     elif x == 5:
                         ans += 50
                 return ans
-    def pair_helper(dic):
-        for k, v in dic.items():
-            c = 0 
-            if v == 2:
-                c += 1
-        return True
     if pair_helper(key):
         return 750
-    def two3(dic):
-        c = 0
-        tot = 0
-        for k, v in dic.items():
-            if v == 3:
-                c +=1
-                if k != 1:
-                    tot += k * 100
-                else:
-                    tot += 1000
-        if c == 2:
-            return tot
-        if two3(key) > 0:
-            return two3(key)
     for k, v in key.items():
         if v == 3:
             if k != 1:
@@ -86,24 +100,9 @@ def dice(arr):
                     elif x == 5:
                         ans += 50
             return ans
-    def last_helper(arr):
-        tot = 0
-        for a in arr:
-            if a == 1:
-                tot += 100
-            elif a == 5:
-                tot += 50
-        return tot
     if last_helper(arr) > 0:
         return last_helper(arr)
     return ans
 
-            
 
-        
-
-    
-        
-        
-
-print(dice([4, 4, 4, 4, 4, 1]))
+print(dice([2,2,3,3,6,6]))
